2d8d4ee099ac2fb1ecf484388c9fc0ac1994d7c1
Keirn H <78835210+Skelpo1@users.noreply.github.com>
2024-12-26 02:31:01 +0000

    Update mac_flood.py

--- .py/mac_flood.py
+++ .py/mac_flood.py
@@ -1,6 +1,6 @@
 from scapy.all import Ether, sendp
 
 for i in range(1000):
-  pkt = Ether(src="00:11:22:33:44:55" / "Flooding"
+  pkt = Ether(src="00:11:22:33:44:55" / "Flooding" # !!!caution when setting mac address as you do not want to set make address flood = 00:00:00:... as this will cause STP confusion!!!
 
               send(pkt, iface="<interface value (eht0)", verbose=0)

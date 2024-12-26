4bbb437b1fbb2019f69d1c4e0b291383a8b6bf99
Keirn H <78835210+Skelpo1@users.noreply.github.com>
2024-12-26 02:28:59 +0000

    Update mac_flood.py

--- .py/mac_flood.py
+++ .py/mac_flood.py
@@ -3,4 +3,4 @@ from scapy.all import Ether, sendp
 for i in range(1000):
   pkt = Ether(src="00:11:22:33:44:55" / "Flooding"
 
-              send(pkt, iface="", verbose=0)
+              send(pkt, iface="<interface value (eht0)", verbose=0)
